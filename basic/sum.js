e513e0c59c55688a40147d008b29dba1da17b3d8
ashleylai58 <ashleylai58@gmail.com>
2022-05-08 14:26:00 +0800

    add sum.js

---
+++ basic/sum.js
@@ -0,0 +1,10 @@
+// 回傳 1 + 2 + ... + n 的結果
+function sum(n) {
+    
+  // return 結果
+}
+
+console.log(sum(1)); // 1
+console.log(sum(2)); // 3
+console.log(sum(10)); // 55
+console.log(sum(100)); // 5050
