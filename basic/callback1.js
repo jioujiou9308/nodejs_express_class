50c7408a4cb0e6680f3d5e8badb8b83f3a1e2fe3
ashleylai58 <ashleylai58@gmail.com>
2022-05-14 16:11:34 +0800

    callback hell

--- basic/callback1.js
+++ basic/callback1.js
@@ -13,13 +13,12 @@ let doWork = function (job, timer, cb) {
 // 刷牙 (3000) -> 吃早餐 (5000) -> 寫功課 (3000)
 
 doWork('刷牙', 3000, function (result) {
+  // 這裡就是刷完牙了
   console.log(result);
-});
-
-doWork('吃早餐', 5000, function (result) {
-  console.log(result);
-});
-
-doWork('寫功課', 3000, function (result) {
-  console.log(result);
+  doWork('吃早餐', 5000, function (result) {
+    console.log(result);
+    doWork('寫功課', 3000, function (result) {
+      console.log(result);
+    });
+  });
 });
