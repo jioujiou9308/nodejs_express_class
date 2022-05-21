a7193bcd74607617871b41bf91e3c0b9911486e8
ashleylai58 <ashleylai58@gmail.com>
2022-05-21 15:26:02 +0800

    promise in map

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -1,5 +1,7 @@
 // read stock no from mysql database
 
+const axios = require('axios');
+
 // mysql2 是一個第三方套件
 // npm i mysql2
 // 引用進來
@@ -20,14 +22,47 @@ require('dotenv').config();
   });
 
   let [data, fields] = await connection.execute('SELECT * FROM stocks');
-  console.log(data);
-
+  // console.log(data);
   // results [
   //     [],
   //     []
   // ]
   //let data = results[0];
   //let fields = results[1];
+
+  /********** for 迴圈版本  ********************/
+  // for (let i = 0; i < data.length; i++) {
+  //   let response = await axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+  //     params: {
+  //       response: 'json',
+  //       date: '20220301',
+  //       stockNo: data[i].id,
+  //     },
+  //   });
+  //   console.log(response.data);
+  // }
+  /****************************************** */
+
+  /************* map 版本 ************************** */
+  let mapResult = data.map(async (stock) => {
+    let response = await axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+      params: {
+        // 設定 query string
+        response: 'json',
+        date: '20220301',
+        stockNo: stock.id,
+      },
+    });
+    return response.data;
+  });
+
+  // mapResult
+  // [ Promise { <pending> }, Promise { <pending> }, Promise { <pending> } ]
+
+  console.log(mapResult);
+  let priceResults = await Promise.all(mapResult);
+  // console.log(priceResults);
+  /****************************************** */
 
   connection.end();
 })();
