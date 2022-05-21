4c822b70c34cf0f0c4f1eae4ecfc9262ced68dac
ashleylai58 <ashleylai58@gmail.com>
2022-05-21 14:14:45 +0800

    add dotenv for crawler

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -4,14 +4,19 @@
 // npm i mysql2
 // 引用進來
 const mysql = require('mysql2/promise');
+// const dotenv = require('dotenv');
+// dotenv.config();
+// 幫我們去把 .env 裡的變數讀進來
+require('dotenv').config();
 
 (async () => {
+  console.log('DB_HOST', process.env.DB_HOST);
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'tester',
-    password: '',
-    database: 'stock_mfee22',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 
   let [data, fields] = await connection.execute('SELECT * FROM stocks');
