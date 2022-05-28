dbbc925381f911250d196bf8062a85550386b88f
ashleylai58 <ashleylai58@gmail.com>
2022-05-28 10:50:51 +0800

    404 & 錯誤處理

--- simple-express/server.js
+++ simple-express/server.js
@@ -18,9 +18,13 @@ const app = express();
 // 1. next: 往下一個中間件去
 // 2. response: 結束這次的旅程 (req-res cycle)
 
+// SSR
+// CSR
+
 // 一般中間件
 app.use((request, response, next) => {
   console.log('我是一個沒有用的中間件 AAAA');
+  next();
 
   // 兩個都有，那會發生什麼事？
   // 情況 1:
@@ -51,6 +55,31 @@ app.get('/', (request, response, next) => {
 app.get('/about', (request, response, next) => {
   console.log('about');
   response.send('About Me');
+});
+
+app.get('/error', (req, res, next) => {
+  // 發生錯誤，你丟一個錯誤出來
+  // throw new Error('測試測試');
+  // 或是你的 next 裡有任何參數
+  next('我是正確的');
+  // --> 都會跳去錯誤處理中間件
+});
+
+// 這個中間件在所有路由的後面
+// 會到這裡，表示前面所有的路由中間件都沒有比到符合的網址
+// => 404
+app.use((req, res, next) => {
+  console.log('所有路由的後面 ==> 404', req.path);
+  res.status(404).send('Not Found');
+});
+
+// 5xx
+// 錯誤處理中間件: 通常也會放在所有中間件的最後
+// 超級特殊的中間件
+// 有點接近 try-catch 的 catch
+app.use((err, req, res, next) => {
+  console.error('來自四個參數的錯誤處理中間件', req.path, err);
+  res.status(500).send('Server Error: 請洽系統管理員');
 });
 
 app.listen(3001, () => {
