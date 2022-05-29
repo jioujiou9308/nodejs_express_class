d2c2855d3858c00f9121999760a4080131dc2c4c
ashleylai58 <ashleylai58@gmail.com>
2022-05-29 14:19:23 +0800

    stock details: add pagination

--- simple-express/server.js
+++ simple-express/server.js
@@ -24,6 +24,7 @@ let pool = mysql
     database: process.env.DB_NAME,
     // 為了 pool 新增的參數
     connectionLimit: 10,
+    dateStrings: true,
   })
   .promise();
 
@@ -50,7 +51,7 @@ app.set('view engine', 'pug');
 
 // extended: false --> querystring
 // extended: true --> qs
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 // express 處理靜態資料
 // 靜態資料: html, css 檔案, javascript 檔案, 圖片, 影音檔...
@@ -127,28 +128,50 @@ app.get('/stocks/:stockId', async (req, res, next) => {
   // 取得網址上的參數 req.params
   // req.params.stockId
   console.log('get stocks by id', req.params);
-  let [data, fields] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id = ?', [req.params.stockId]);
-  // console.log('query stock by id:', data);
 
   // RESTful 風格之下，鼓勵把這種過濾參數用 query string 來傳遞
   // /stocks/:stockId?page=1
-  // 取得目前在第幾頁，而且利用 || 這個特性來做預設值
+  // 1. 取得目前在第幾頁，而且利用 || 這個特性來做預設值
+  // req.query = {}
   // 如果網址上沒有 page 這個 query string，那 req.query.page 會是 undefined
   // undefined 會是 false，所以 page 就被設定成 || 後面那個數字
   // https://developer.mozilla.org/zh-CN/docs/Glossary/Falsy
   let page = req.query.page || 1;
   console.log('current page', page);
 
-  // TODO: 取得目前的總筆數
-  // TODO: 計算總共有幾頁
-  // TODO: 計算 offset 是多少（計算要跳過幾筆）
-  // TODO: 取得這一頁的資料 select * ... limit ? offet ?
-  // TODO: 回覆給前端
+  // 2. 取得目前的總筆數
+  let [allResults, fields] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id = ?', [req.params.stockId]);
+  const total = allResults.length;
+  console.log('total:', total);
+
+  // 3. 計算總共有幾頁
+  // Math.ceil 1.1 => 2   1.05 -> 2
+  const perPage = 5; // 每一頁有幾筆
+  const lastPage = Math.ceil(total / perPage);
+  console.log('lastPage:', lastPage);
+
+  // 4. 計算 offset 是多少（計算要跳過幾筆）
+  // 在第五頁，就是要跳過 4 * perPage
+  let offset = (page - 1) * perPage;
+  console.log('offset:', offset);
+
+  // 5. 取得這一頁的資料 select * from table limit ? offet ?
+  let [pageResults] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id = ? ORDER BY date DESC LIMIT ? OFFSET ?', [req.params.stockId, perPage, offset]);
+
+  // test case:
+
+  // 正面: 沒有page, page=1, page=2, page=12 (因為總共12頁)
+  // 負面: page=-1, page=13, page=空白(page=1), page=a,...
+  // 6. 回覆給前端
   res.json({
     // 用來儲存所有跟頁碼有關的資訊
-    pagination: {},
+    pagination: {
+      total,
+      lastPage,
+      page,
+    },
     // 真正的資料
-    data: data,
+    data: pageResults,
   });
 });
 
