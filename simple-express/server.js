588cf790b85eed44bf138b262e5b651bd4283297
ashleylai58 <ashleylai58@gmail.com>
2022-05-29 11:45:54 +0800

    stock details: get current page

--- simple-express/server.js
+++ simple-express/server.js
@@ -48,6 +48,10 @@ app.set('views', path.join(__dirname, 'views'));
 // npm i pug
 app.set('view engine', 'pug');
 
+// extended: false --> querystring
+// extended: true --> qs
+app.use(express.urlencoded({ extended: true }));
+
 // express 處理靜態資料
 // 靜態資料: html, css 檔案, javascript 檔案, 圖片, 影音檔...
 // express 少數內建的中間件 static
@@ -123,18 +127,29 @@ app.get('/stocks/:stockId', async (req, res, next) => {
   // 取得網址上的參數 req.params
   // req.params.stockId
   console.log('get stocks by id', req.params);
-  let [data, fields] = await pool.execute('SELECT * FROM stocks WHERE id = ?', [req.params.stockId]);
+  let [data, fields] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id = ?', [req.params.stockId]);
+  // console.log('query stock by id:', data);
 
-  console.log('query stock by id:', data);
-  // 空資料(查不到資料)有兩種處理方式：
-  // 1. 200OK 就回 []
-  // 2. 回覆 404
-  if (data.length === 0) {
-    // 這裡是 404 範例
-    res.status(404).json(data);
-  } else {
-    res.json(data);
-  }
+  // RESTful 風格之下，鼓勵把這種過濾參數用 query string 來傳遞
+  // /stocks/:stockId?page=1
+  // 取得目前在第幾頁，而且利用 || 這個特性來做預設值
+  // 如果網址上沒有 page 這個 query string，那 req.query.page 會是 undefined
+  // undefined 會是 false，所以 page 就被設定成 || 後面那個數字
+  // https://developer.mozilla.org/zh-CN/docs/Glossary/Falsy
+  let page = req.query.page || 1;
+  console.log('current page', page);
+
+  // TODO: 取得目前的總筆數
+  // TODO: 計算總共有幾頁
+  // TODO: 計算 offset 是多少（計算要跳過幾筆）
+  // TODO: 取得這一頁的資料 select * ... limit ? offet ?
+  // TODO: 回覆給前端
+  res.json({
+    // 用來儲存所有跟頁碼有關的資訊
+    pagination: {},
+    // 真正的資料
+    data: data,
+  });
 });
 
 // 這個中間件在所有路由的後面
